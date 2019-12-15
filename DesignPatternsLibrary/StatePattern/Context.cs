b7599ac1a762bd76b62de7345ba5f8946c6802a7
Osirisoo0O <setzero@live.cn>
2019-12-15 14:00:04 +0800

    添加项目文件。

---
+++ DesignPatternsLibrary/StatePattern/Context.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatternsLibrary.StatePattern
+{
+    /// <summary>
+    /// 状态拥有者
+    /// </summary>
+   public class Context
+    {
+        State _State = null;
+        /// <summary>
+        /// 状态方法调用
+        /// </summary>
+        /// <param name="value"></param>
+        public void Request(int value)
+        {
+            _State.handle(value);
+        }
+        /// <summary>
+        /// 状态设置
+        /// </summary>
+        /// <param name="theState"></param>
+        public void SetState(State theState)
+        {
+            _State = theState;
+        }
+    }
+}
---
+++ DesignPatternsLibrary/StatePattern/State.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatternsLibrary.StatePattern
+{
+    /// <summary>
+    /// 状态接口
+    /// </summary>
+   public abstract class State
+    {
+        protected Context _Context = null;
+        public State(Context context)
+        {
+            _Context = context;
+        }
+        public abstract void handle(int value);
+    }
+}
