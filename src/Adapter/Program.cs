65900e09f3e43f53ac2838598d2c953365bdde68
Osirisoo0O <setzero@live.cn>
2020-03-06 20:45:04 +0800

    Add Adapter

---
+++ src/Adapter/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Adapter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IThreeHole threehole = new PowerAdapter();
+            threehole.Request();
+            Console.ReadLine();
+        }
+    }
+}
