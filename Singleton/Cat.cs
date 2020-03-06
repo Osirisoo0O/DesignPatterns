348ea2257adde57e26730f6d645c56db40c93629
Osirisoo0O <setzero@live.cn>
2020-03-06 15:43:08 +0800

    Add Singleton

---
+++ Singleton/Cat.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Singleton
+{
+    /// <summary>
+    /// 猫
+    /// </summary>
+    public class Cat
+    {
+        private static Cat instance = null;
+        private static readonly object locker = new object();
+
+        /// <summary>
+        /// 双层锁实例化
+        /// </summary>
+        public static Cat Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    lock (locker)
+                    {
+                        if (instance == null)
+                        {
+                            instance = new Cat();
+                        }
+                    }
+                }
+                return instance;
+            }
+        }
+
+        /// <summary>
+        /// 猫叫
+        /// </summary>
+        /// <returns></returns>
+        public string Miao()
+        {
+            return "Miao~";
+        }
+    }
+}
