63f6cb2045dd6aa4c392ddb134bd2f2d5ced8530
Osirisoo0O <setzero@live.cn>
2020-03-06 20:44:51 +0800

    Add Bridge

---
+++ src/Bridge/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Bridge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RemoteControl rc = new ConcreteRemoteControl();
+
+            rc.Implementor = new TCL();
+            rc.On();
+            rc.SetChannel();
+            rc.Off();
+            Console.WriteLine("------------------------");
+
+            rc.Implementor = new Samsung();
+            rc.On();
+            rc.SetChannel();
+            rc.Off();
+            Console.Read();
+        }
+    }
+}
---
+++ src/Bridge/PublicClass.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bridge
+{
+    public abstract class RemoteControl
+    {
+
+        private TV _Implementor;
+        public TV Implementor
+        {
+            get { return _Implementor; }
+            set
+            {
+                _Implementor = value;                
+            }
+        }
+        public virtual void On()
+        {
+            Implementor.On();
+        }
+        public virtual void Off()
+        {
+            Implementor.Off();
+        }
+        public virtual void SetChannel()
+        {
+            Implementor.TurnChannel();
+        }
+    }
+    public class ConcreteRemoteControl : RemoteControl
+    {
+        public override void SetChannel()
+        {
+            Console.WriteLine("---------------------");
+            base.SetChannel();
+            Console.WriteLine("---------------------");
+        }
+    }
+
+    public abstract class TV
+    {
+        public abstract void On();
+        public abstract void Off();
+        public abstract void TurnChannel();
+    }
+
+    public class TCL : TV
+    {
+        public override void Off()
+        {
+            Console.WriteLine("TCL电视关闭了");
+        }
+
+        public override void On()
+        {
+            Console.WriteLine("TCL电视打开了");
+        }
+
+        public override void TurnChannel()
+        {
+            Console.WriteLine("TCL电视换频道");
+        }
+    }
+    public class Samsung : TV
+    {
+        public override void Off()
+        {
+            Console.WriteLine("Samsung 电视关闭了");
+        }
+
+        public override void On()
+        {
+            Console.WriteLine("Samsung 电视打开了");
+        }
+
+        public override void TurnChannel()
+        {
+            Console.WriteLine("Samsung 电视换频道");
+        }
+    }
+}
