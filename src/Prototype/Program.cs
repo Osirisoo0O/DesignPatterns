d4a9baebc810961ca4f09465f95a53a98555d768
Osirisoo0O <setzero@live.cn>
2020-03-06 18:40:14 +0800

    Add Prototypr

---
+++ src/Prototype/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Prototype
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MonkeyKing monkey = new ConcreteClone("MonkeyKing");
+            for(int i = 0; i < 10; i++)
+            {
+            MonkeyKing clone=monkey.Clone() as ConcreteClone;
+            Console.WriteLine($"---Cloned{i}:\t{clone.Id}  第{i+1}个");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ src/Prototype/PublicClass.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Prototype
+{
+    public abstract class MonkeyKing
+    {
+        public string Id { get; set; }
+        public MonkeyKing(string Id)
+        {
+            this.Id = Id;
+        }
+
+
+        public abstract MonkeyKing Clone();
+    }
+
+    public class ConcreteClone : MonkeyKing
+    {
+        public ConcreteClone(string Id):base(Id)
+        {           
+        }
+
+        public override MonkeyKing Clone()
+        {
+            return (MonkeyKing)this.MemberwiseClone(); //浅拷贝
+        }
+    }
+}
