e95c704d40afdf0acdfca8a77d8bdb66e48a2e41
Osirisoo0O <setzero@live.cn>
2021-06-30 15:02:52 +0800

    Add Composite

---
+++ src/Composite/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Composite
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ComplexGraphics complex = new ComplexGraphics("一个复杂图形和两条线段");
+
+            complex.Add(new Line("线段A"));
+            ComplexGraphics complex1 = new ComplexGraphics("复杂图形");
+            complex1.Add(new Circle("圆"));
+            complex1.Add(new Line("线段B"));
+            complex.Add(complex1);
+            Line l = new Line("线段C");
+            complex.Add(l); 
+
+
+            Console.WriteLine("复杂图形画法如下：");
+            Console.WriteLine("--------------");
+            complex.Draw();
+            Console.WriteLine("复杂图形绘制完成");
+            Console.WriteLine("--------------");
+            Console.WriteLine();
+
+            complex.Remove(l); 
+            Console.WriteLine("移除C后复杂图形画法如下：");
+            Console.WriteLine("--------------");
+            complex.Draw();
+            Console.WriteLine("复杂图形绘制完成");
+            Console.WriteLine("--------------");
+            Console.WriteLine();
+        }
+    }
+}
---
+++ src/Composite/PublicClass.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Composite
+{
+   public abstract class Graphics
+    {
+        public string Name { get; set; }
+
+        public Graphics(string name)
+        {
+            this.Name = name;
+        }
+
+        public abstract void Draw();
+        //public abstract void Add(Graphics g);
+        //public abstract void Remove(Graphics g);
+    }
+
+    public class Line : Graphics
+    {
+
+        public Line(string name):base(name)
+        {
+
+        }
+
+        public override void Draw()
+        {
+            Console.WriteLine("画" + Name);
+        }
+        //public override void Add(Graphics g)
+        //{
+        //    throw new Exception("不能向简单图形Line添加其他图形");
+        //}
+
+        //public override void Remove(Graphics g)
+        //{
+        //    throw new Exception("不能向简单图形Line移除其他图形");
+        //}
+    }
+
+    public class Circle : Graphics
+    {
+        public Circle(string name):base(name)
+        {
+
+        }
+        public override void Draw()
+        {
+            Console.WriteLine("画"+Name);
+        }
+
+        //public override void Add(Graphics g)
+        //{
+        //    throw new Exception("不能向简单图形Circle添加其他图形");
+        //}
+
+
+        //public override void Remove(Graphics g)
+        //{
+        //    throw new Exception("不能向简单图形Circle移除其他图形");
+        //}
+    }
+
+    public class ComplexGraphics : Graphics
+    {
+        private readonly List<Graphics> Graphicses = new List<Graphics>();
+        public ComplexGraphics(string name):base(name)
+        {
+
+        }
+
+
+        public override void Draw()
+        {
+            foreach (var item in Graphicses)
+            {
+                item.Draw();
+            }
+        }
+        public void Add(Graphics g)
+        {
+            Graphicses.Add(g);
+        }
+
+        public void Remove(Graphics g)
+        {
+            Graphicses.Remove(g);
+        }
+    }
+}
