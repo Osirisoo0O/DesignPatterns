504d276f647f89a9990b646203f3838904dd5800
Osirisoo0O <setzero@live.cn>
2021-06-30 14:40:11 +0800

    Add Decorator

---
+++ src/Decorator/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Decorator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Phone phone = new iPhone();
+
+            Decorator iPhoneWithSticker = new Sticker(phone);
+            iPhoneWithSticker.Print();
+            Console.WriteLine("-----------------");
+
+            Decorator iPhoneWithAccessories = new Accessories(phone);
+            iPhoneWithAccessories.Print();
+            Console.WriteLine("-----------------");
+
+
+            Sticker sticker = new Sticker(phone);
+            Accessories iPhoneWithStickerAndAccessoies = new Accessories(sticker);
+            iPhoneWithStickerAndAccessoies.Print();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ src/Decorator/PublicClass.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Decorator
+{
+    /// <summary>
+    /// 手机抽象类
+    /// </summary>
+    public abstract class Phone
+    {
+        public abstract void Print();
+    }
+
+    public class iPhone : Phone
+    {
+        public override void Print()
+        {
+            Console.WriteLine("开始执行具体对象：iPhone");
+        }
+    }
+
+    public abstract class Decorator : Phone
+    {
+        private Phone phone;
+
+        public Decorator(Phone p)
+        {
+            this.phone = p;
+        }
+        public override void Print()
+        {
+            if (phone!=null)
+            {
+                phone.Print();
+            }
+        }
+
+    }
+
+    public class Sticker : Decorator
+    {
+        public Sticker(Phone p):base(p)
+        {
+
+        }
+
+        public override void Print()
+        {
+            base.Print();
+            AddSticker();
+        }
+
+        private void AddSticker()
+        {
+            Console.WriteLine("iPhone已贴膜");
+        }
+    }
+
+    public class Accessories : Decorator
+    {
+        public Accessories(Phone p):base(p)
+        {
+
+        }
+        public override void Print()
+        {
+            base.Print();
+
+            AddAccessories();
+        }
+
+        private void AddAccessories()
+        {
+            Console.WriteLine("iPhone已有挂件");
+        }
+    }
+
+
+
+
+
+
+
+
+
+}
